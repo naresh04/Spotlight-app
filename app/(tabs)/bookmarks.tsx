e081687c7e66a38c20c5cc113e1ccbb95bf202ea
vn576fg <Naresh.chouhan@walmart.com>
2025-04-20 10:23:35 -0400

    first session

---
+++ app/(tabs)/bookmarks.tsx
@@ -0,0 +1,10 @@
+import { Text,View } from "react-native";
+
+export default function bookmarks(){
+    return(
+        <View>
+            <Text>Create</Text>
+        </View>
+    )
+
+}
---
+++ app/(tabs)/notifications.tsx
@@ -0,0 +1,12 @@
+import { View,Text } from "react-native";
+
+
+export default function Notifications(){
+    return(
+        <View>
+            <Text>
+               Notifcations 
+            </Text>
+        </View>
+    )
+}
